cbf482588d4ac03b1c1b5586c48792e27ebbb7eb
graupv <pin18848@uvg.edu.gt>
2019-02-22 21:53:43 -0600

    update simulations

--- src/Main/simulations.py
+++ src/Main/simulations.py
@@ -1,16 +1,32 @@
 
 import simpy
 import random
+import timeit
 
 
-totalDia = None
-def process(env, ram, cpu):
+
+class Resultado:
+    totes = 0
+
+    @classmethod
+    def set_totes(self, n):
+        self.totes = n
+
+
+
+def thing(env, nram, ram, cpu, inst):
+    ready = False
     #   definir como va a fucionar la cola
     #   pedir la ram si hay hacer el proceso si no esperar
     #   devolver resultado
-
-    # if RAM
     
+    # if RAM
+    start = env.now
+    with ram.request() as mem:
+        if mem > 0:
+            ready = True
+            yield mem
+        
     pass
            
 
@@ -20,16 +36,16 @@ env = simpy.Environment()
 #   ambiente de simulacion
 RAM = simpy.Container(env, capacity=100, init=100)
 #   100 Unidades de RAM
-CPU = simpy.Resource(env, capacity=1)
-#   1 Core
-Queue = simpy.Resource(env, capacity=1) # ?
-random.seed(10) # fijar el inicio de random
+CPU = simpy.Resource(env, capacity=1)   
+#   "Podra incrementarse", significa + capacity?
+#   simpy.Resource == Shared resources inside environment
+random.seed(666) # fijar el inicio de random
 
 x = 25
 #   25, 50, 100, 200, 250
 for i in range(x):
-    env.process(process(env, RAM, CPU))
+    env.process(thing(env, ram=RAM, cpu=CPU, nram=random.randint(1, 10), inst=random.randint(1, 3)))
+    #   instanciamos el objeto a simular -> se agrega a environment 
+    #   (simpy env, resource_ram, resource_cpu, cantidad de ram necesaria, cantidad de intrucciones a realizar)
 
 env.run(until=50)  #correr la simulaci�n hasta el tiempo = 50
-
-print ("tiempo promedio por vehiculo es: ", totalDia/x)
