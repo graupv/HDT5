c5f6d9916f7754c9e86cd1c323b88124007ca8a9
graupv <pin18848@uvg.edu.gt>
2019-02-25 11:55:57 -0600

    simulaciones fin
    Para correr usar run()
    pediría numero de procesos a simular y cantidad de tiempo de simulacion.
    
    Falta:
    - Calcular desviacion estandar
    - Hacer las simulaciones con los valores pedidos (25, 50... 200)
    - Hacer simulacion con diferente patron de generacion (1/10 -> 1/5 -> 1/1)
    - Hacer simulacion con 2 CPU's
    (CPU resource capacity =2)
    - Graficas con todos los datos

--- src/Main/simulations.py
+++ src/Main/simulations.py
@@ -1,36 +1,36 @@
+'''
+Hoja de Trabajo 5   -   Simulaciones discretas en Simpy
+
+Kristen Brandt - 1
+Gerardo Pineda - 18848
+
+'''
 
 import simpy
 import random
-import timeit
 
-class Computer:
-
-    def __init__(self, env):
-        all_the_times = []
-        self.cpu = simpy.Resource(env, capacity=1)
-        self.ram = simpy.Container(env, capacity=100, init=100)
-
-    def buf(self, x):
-        return self.ram.level >= x
-
-class Resultados:
-    totes = 0
-    mcgoats = []
+class Data:
+    res = []
+    std = 0
+    avg = 0
 
     @classmethod
-    def set_totes(self, n):
-        self.totes += n
+    def get_avg(self):
+        self.avg = sum(self.res) / len(self.res)
+        return self.avg
 
     @classmethod
-    def add_goats(self, n):
-        self.mcgoats.append(n)
+    def get_stdv(self):
+
+        return
 
 def print_stats(res):
     print('%d of %d slots are allocated.' % (res.count, res.capacity))
     print('  Users:', res.users)
     print('  Queued events:', res.queue)
 
-def proceso(env, nram, ram, cpu, inst):   
+def proceso(env, nram, ram, cpu, inst):
+    print('Instructions:', inst)
     start = env.now #   tiempo en que instancia el proceso
     while ram.level < nram:
         #   "cola"
@@ -38,40 +38,77 @@ def proceso(env, nram, ram, cpu, inst):
         #   esperar un ciclo de reloj mientras se libera memoria
         #   yield para devolver control al ciclo
 
-    gotMem = env.now - start
     yield ram.get(nram)
+    gotMem = env.now - start
+    print('got mem:', gotMem)
     #   allocate ram
-    with cpu.request() as proc:
-        while inst > 2:
+    #   new -> Ready
+    print('***Before cpu request:\n'.upper())
+    print_stats(cpu)
+
+    while inst > 2:
+        with cpu.request() as proc:
+        
+            print('***Requested CPU:\n'.upper())
+            print_stats(cpu)
+            yield proc
+            yield env.timeout(1)    
             inst -= 3
-            yield env.timeout(1)
-            #   devuelve el CPU por 1 ciclo de reloj
+            #   simula procesamiento de 3 instrucciones
+            print('***Post Yield, inst -3:'.upper())
+            print('***inst left:', inst, '\n')
+            print_stats(cpu)
+            #   fin with devuelve el CPU por ciclo de reloj
             
-        print_stats(proc)
-        Resultados.add_goats(env.now - gotMem)
-        # timeout == tiempo de realizar proceso
-    #   finalizado el proceso, devolver ram
-    # Resultados.mcgoats(env.now - gotMem)
-    yield ram.set(nram)
+        print('***Outside WITH, releasing cpu:\n'.upper())
+        print_stats(cpu)
+        #   random int para ver si va a cola de waiting o regresa a ready
+        if random.randint(1, 2) == 2:
+            yield env.timeout(1)
+            #   simular operaciones I/O, regresa a cola para pedir cpu luego de 1 ciclo
+    print('***Outside while, process Terminated:\n'.upper())
+    print('final time:', (env.now))
+    Data.res.append(env.now - start)
+    yield ram.put(nram) #   devolver memoria utilizada
+    env.exit()  #   finalizado proceso, terminado.
+    
+
+def process_gen(env, RAM, CPU, procesos=25):
+    for x in range(procesos):
+        yield env.timeout(random.expovariate(1/10))
+        print("Process #", x + 1)
+        env.process(proceso(env, ram=RAM, cpu=CPU, nram=random.randint(1, 10), inst=random.randint(1, 10)))
 
 # ----------------------
 
-env = simpy.Environment() 
-#   ambiente de simulacion
-RAM = simpy.Container(env, capacity=100, init=100)
-#   100 Unidades de RAM
-CPU = simpy.Resource(env, capacity=1)
-#   "Podra incrementarse", significa + capacity?
+
 
 random.seed(666) # fijar el inicio de random
 
-x = 25
+from math import sqrt
+def run():
+    Data.res = []
+    env = simpy.Environment()
+    #   ambiente de simulacion
+    RAM = simpy.Container(env, capacity=100, init=100)
+    #   100 Unidades de RAM
+    CPU = simpy.Resource(env, capacity=1)
+    #   "Podra incrementarse", significa + capacity?
+    p = int(input("Cuantos procesos desea simular: "))
+    env.process(process_gen(env, RAM, CPU, p))
+    x = int(input("Simulation time: "))
+    env.run(until=x)
+    Data.avg = sum(Data.res) / p
+    print('Promedio:', Data.get_avg())
+    # std = 0
+    # for val in Data.res:
+    #     sqrt((val - Data.avg)**2)
+    # print('Desviacion estandar:', sum(Data.res) / p)
 #   25, 50, 100, 200, 250
-for i in range(x):
-    #   falta setear el ritmo al que se generan
-    #   es el random.expovariate(n/10)
-    env.process(proceso(env, ram=RAM, cpu=CPU, nram=random.randint(1, 10), inst=random.randint(1, 10)))
-    #   instanciamos el objeto a simular -> se agrega a environment 
-    #   (simpy env, resource_ram, resource_cpu, cantidad de ram necesaria, cantidad de intrucciones a realizar)
+#   100 procesos necesita 800 simulation time
 
-env.run(until=50)  #correr la simulaci�n hasta el tiempo = 50
+#   instanciamos el objeto a simular -> se agrega a environment 
+#   (simpy env, container_ram, resource_cpu, cantidad de ram necesaria, cantidad de intrucciones a realizar)
+
+# env.run(until=50)
+# print('final time', t)
